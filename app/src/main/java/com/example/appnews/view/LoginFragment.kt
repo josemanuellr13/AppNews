3a5baaba9623b6aaca2069054d0f85d1a80b0193
josemanuellr13 <61747423+josemanuellr13@users.noreply.github.com>
2023-01-23 10:29:24 +0100

    up

--- app/src/main/java/com/example/appnews/view/LoginFragment.kt
+++ app/src/main/java/com/example/appnews/view/LoginFragment.kt
@@ -6,13 +6,16 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.appnews.R
 import com.example.appnews.databinding.FragmentLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-
+import androidx.navigation.ui.setupWithNavController
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
     private lateinit var binding : FragmentLoginBinding
@@ -30,13 +33,21 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         //val btnRedirect = binding.tvRedirectRegister
 
 
+        val navController = findNavController()
+        //navController.navigate(R.id.registerFragment)
+       // navController.navigate(action)
+
+
         tvCrearCuenta.setOnClickListener(){
+            navController.navigate(R.id.registerFragment)
+            /*
             val newFragment = RegisterFragment()
             val fragmentManager = fragmentManager
             val fragmentTransaction = fragmentManager!!.beginTransaction()
-            fragmentTransaction.replace(R.id.action_loginFragment_to_registerFragment, newFragment)
+            fragmentTransaction.replace(R.id.loginFragment, newFragment)
             fragmentTransaction.addToBackStack(null)
             fragmentTransaction.commit()
+            navController.navigate(action)*/
         }
 
 
@@ -46,6 +57,7 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
                     auth.signInWithEmailAndPassword(tvEmail.text.toString(), tvPassword.text.toString())
                         .addOnCompleteListener(it1){ task ->
                             if(task.isSuccessful){
+
                                 Toast.makeText(context, "Usuario correcto", Toast.LENGTH_SHORT).show()
                                 val intent = Intent(context, MainHome::class.java)
                                 startActivity(intent)
@@ -56,8 +68,8 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
                         }
                 }
 
-            }else{
-                Log.i("E","No se pudo iniciar sesion")
+            }else if(!tvEmail.text.toString().isNotEmpty() || !tvPassword.text.toString().isNotEmpty()){
+                Toast.makeText(context,  "Debes rellenar todos los datos", Toast.LENGTH_LONG).show()
             }
         }
     }
--- app/src/main/java/com/example/appnews/view/RegisterFragment.kt
+++ app/src/main/java/com/example/appnews/view/RegisterFragment.kt
@@ -1,21 +1,60 @@
 package com.example.appnews.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.appnews.R
-import com.example.appnews.databinding.FragmentLoginBinding
 import com.example.appnews.databinding.FragmentRegistroBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class RegisterFragment : Fragment(R.layout.fragment_registro) {
 
     private lateinit var binding : FragmentRegistroBinding
+    private lateinit var auth : FirebaseAuth
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentRegistroBinding.bind(view)
+
+        auth = Firebase.auth
+
+        val email = binding.email
+        val clave1 = binding.clave
+        val clave2 = binding.clave2
+        val btnRegistrar = binding.btnRegistrar
+
+        // Al pulsar btn de registrar
+        btnRegistrar.setOnClickListener(){
+            RegisterFragment().activity?.let { it1 ->
+
+                // Si los campos no están rellenos
+                if(!email.toString().isNotEmpty() && !clave1.toString().isNotEmpty() && !clave2.toString().isNotEmpty()){
+                    Toast.makeText(context, "Debes rellenar todos los campos", Toast.LENGTH_SHORT).show()
+
+                // Si las claves no coinciden
+                }else if(clave1.toString().isNotEmpty() != clave2.toString().isNotEmpty()){
+                    Toast.makeText(context, "Las claves no coinciden", Toast.LENGTH_SHORT).show()
+
+                // To.do correcto
+                }else{
+                    auth.createUserWithEmailAndPassword(email.toString(),email.toString())
+                        .addOnCompleteListener(it1){ task ->
+                            if(task.isSuccessful) {
+                                Toast.makeText(context, "Registro correcto", Toast.LENGTH_SHORT).show()
+                            }else{
+                                Toast.makeText(context, "Algo falló", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                }
+            }
+        }
+
+
     }
 
 }
